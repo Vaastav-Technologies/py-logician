0a1e3722a5551f7d44723e3edd8296c77095995e
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-03-26 23:59:07 +0530

    basic-log-test: more log detail.

--- tests/vt/utils/test_logging/test_std_log.py
+++ tests/vt/utils/test_logging/test_std_log.py
@@ -1,7 +1,7 @@
 from vt.utils.logging.std_log.std_log import BasicStdLevelLogger
 import logging
 
-TIMED_DETAIL_LOG_FMT = '%(asctime)s: %(name)s: [%(levelname)s]: [%(filename)s - ' \
+TIMED_DETAIL_LOG_FMT = '%(asctime)s: %(name)s: [%(levelname)s]: [%(filename)s:%(lineno)d - ' \
                        '%(funcName)10s() ]: %(message)s'
 
 def test_initial_logging():
