8d982c9ec372075e33dac3316b29a459d690a0bd
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-03-30 00:19:24 +0530

    basic-log: delegation interfaces exposed.

--- src/vt/utils/logging/logging/delegating/__init__.py
+++ src/vt/utils/logging/logging/delegating/__init__.py
@@ -4,3 +4,7 @@
 """
 Logging implemented using delegating, i.e. loggers can be varied independently.
 """
+
+
+from vt.utils.logging.logging.delegating.base import BaseDelegatingLogger, ProtocolMinLevelLoggerImplBase, \
+    ProtocolMinLevelLoggerImplABC, AllLevelLoggerImplABC
