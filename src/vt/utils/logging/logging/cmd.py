b8e0fb8e464dc9f93c0e5f36e6333417f50f2444
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-02 18:46:19 +0530

    cmd-log: docs added.

--- src/vt/utils/logging/logging/cmd.py
+++ src/vt/utils/logging/logging/cmd.py
@@ -14,13 +14,47 @@ from vt.utils.logging.logging.base import TraceLogProtocol, DebugLogProtocol, In
 
 class MinCmdLogProtocol(LogLogProtocol, DebugLogProtocol, InfoLogProtocol, WarningLogProtocol, ErrorLogProtocol,
                         FatalLogProtocol, Protocol):
+    """
+    Interface for::
+
+        - LOG
+        - DEBUG
+        - INFO
+        - WARNING
+        - ERROR
+        - FATAL
+
+    logging levels.
+    """
     pass
 
 
 class AllCmdLogProtocol(TraceLogProtocol, MinCmdLogProtocol, SuccessLogProtocol, NoticeLogProtocol,
                         ExceptionLogProtocol, Protocol):
+    """
+    Interface for::
+
+        - All ``MinCmdLogProtocol`` log levels.
+        - TRACE
+        - SUCCESS
+        - NOTICE
+        - EXCEPTION
+
+    logging levels.
+
+    see ``MinCmdLogProtocol``.
+    """
     pass
 
 
 class AllLevelCmdLogger(AllCmdLogProtocol, HasUnderlyingLogger, Protocol):
+    """
+    Logger interface for::
+
+        - All ``AllCmdLogProtocol`` log levels.
+
+    logging levels.
+
+    see ``AllCmdLogProtocol``.
+    """
     pass
