a798054e092f8c108410febf8a62ef14201d3c6a
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-03 18:04:52 +0530

    all-log: we do not want full stack-trace for warnings.

--- src/vt/utils/logging/logging/std_log/configurator.py
+++ src/vt/utils/logging/logging/std_log/configurator.py
@@ -31,10 +31,18 @@ class DirectStdLoggerConfigurator(LoggerConfigurator):
             try:
                 int_level = level if isinstance(level, int) else logging.getLevelNamesMapping()[level]
             except KeyError:
-                warnings.warn(f"Undefined log level: '{level}'. Setting log level to default: "
-                              f"'{logging.getLevelName(DirectStdLoggerConfigurator.DEFAULT_LOG_LEVEL)}'.")
+                levels_to_choose_from: list[str] = [logging.getLevelName(level) for level in
+                                                    sorted(logging.getLevelNamesMapping().values())]
+                prev_formatwarning = warnings.formatwarning
+                with warnings.catch_warnings(action="once", category=UserWarning):
+                    warnings.formatwarning = lambda message, category, filename, lineno, line=None: f'{category.__name__}: {message}\n'
+                    warnings.warn(f"Undefined log level: '{level}'. Choose from {levels_to_choose_from}.")
+                    warnings.warn("Setting log level to default: "
+                                  f"'{logging.getLevelName(DirectStdLoggerConfigurator.DEFAULT_LOG_LEVEL)}'.")
+                warnings.formatwarning = prev_formatwarning
                 int_level = DirectStdLoggerConfigurator.DEFAULT_LOG_LEVEL
             hdlr.setFormatter(logging.Formatter(fmt=lvl_fmt_handlr.fmt(int_level)))
+            warnings.warn('Checking warning')
             logger.addHandler(hdlr)
             logger.setLevel(int_level)
         return BaseDirectAllLevelLogger(DirectAllLevelLoggerImpl(logger))
