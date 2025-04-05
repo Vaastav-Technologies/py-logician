c764dfd65f76b1a0d894ac86ff090ed65b219612
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-06 01:52:39 +0530

    warnings: docs improved.

--- src/vt/utils/logging/warnings/__init__.py
+++ src/vt/utils/logging/warnings/__init__.py
@@ -17,6 +17,16 @@ def suppress_warning_stacktrace(fmt: str = "{category}: {message}\n"):
     """
     Context manager to suppress the stack trace for warnings,
     showing only the message with the warning label.
+
+    This context manager allows to alter the warning print format by the caller, following are supported::
+
+        - category - The warning category, e.g. UserWarning.
+        - message - The warning message
+        - filename - The name of the file to be printed with the warning.
+        - lineno - lineno of the warning generator file to be printed with the warning.
+        - line - The warning line.
+
+    :param fmt: Warning print format can be altered by the client code.
     """
     original_format = warnings.formatwarning
     def no_stack_trace(message: Warning | str,
