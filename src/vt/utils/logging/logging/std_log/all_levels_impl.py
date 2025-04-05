573f49e234f491c7660055ae7068ceb3b5deb99e
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-06 02:15:15 +0530

    fixup! all-log: TODO: move TempSetLevelName to utils.
    TODO completed.

--- src/vt/utils/logging/logging/std_log/all_levels_impl.py
+++ src/vt/utils/logging/logging/std_log/all_levels_impl.py
@@ -4,7 +4,6 @@
 """
 Classes w.r.t implementation inheritance are defined here.
 """
-import logging
 import warnings
 from abc import abstractmethod
 from logging import Logger
@@ -14,7 +13,7 @@ from vt.utils.logging.logging.delegating import AllLevelLoggerImplABC
 from vt.utils.logging.logging.std_log import TRACE_LOG_LEVEL, \
     NOTICE_LOG_LEVEL, SUCCESS_LOG_LEVEL, StdLogProtocol, INDIRECTION_STACK_LEVEL, FATAL_LOG_LEVEL, CMD_LOG_LEVEL, \
     CMD_LOG_STR
-from vt.utils.logging.warnings import suppress_warning_stacktrace
+from vt.utils.logging.logging.std_log.utils import TempSetLevelName
 
 
 class StdProtocolAllLevelLoggerImpl(AllLevelLoggerImplABC, Protocol):
@@ -121,50 +120,6 @@ class DirectAllLevelLoggerImpl(BaseDirectStdAllLevelLoggerImpl):
     @override
     def log(self, level: int, msg: str, *args, **kwargs) -> None:
         self.underlying_logger.log(level, msg, *args, stacklevel=self.stack_level, **kwargs)
-
-
-# TODO: move TempSetLevelName to utils.
-class TempSetLevelName:
-    def __init__(self, level: int, level_name: str | None, reverting_lvl_name: str, no_warn: bool = False):
-        """
-        Set the log level name temporarily and then revert it back to the ``reverting_lvl_name``.
-
-        :param level: The log level to set name to.
-        :param level_name: Level name to set the level to.
-        :param reverting_lvl_name: The log level name to revert to when operation finishes.
-        :param no_warn: A warning is shown if the supplied ``level_name`` is strip-empty. This warning can be suppressed
-            by setting ``no_warn=True``.
-        """
-        self.level = level
-        self.level_name = level_name
-        self.reverting_lvl_name = reverting_lvl_name
-        self.no_warn = no_warn
-        self.original_level_name = logging.getLevelName(level)
-
-    def __enter__(self):
-        if self.level_name is not None:
-            if self.level_name.strip() == '':
-                self.warn_user()
-            else:
-                logging.addLevelName(self.level, self.level_name)
-
-    def warn_user(self):
-        """
-        A warning is shown if the supplied ``level_name`` is strip-empty. This warning can be suppressed
-            by setting ``no_warn=True`` in ctor.
-        """
-        if not self.no_warn:
-            with suppress_warning_stacktrace():
-                self._warn_user()
-
-    def _warn_user(self):
-        warnings.warn(f"Supplied log level name for log level {self.level} is empty.")
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.level_name:
-            logging.addLevelName(self.level, self.reverting_lvl_name)
-        else:
-            logging.addLevelName(self.level, self.original_level_name)
 
 
 class TempSetCmdLvlName(TempSetLevelName):
