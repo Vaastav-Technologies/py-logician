0259dfbf8cc324d40e102d7f2e49a8484019c94e
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-09 15:15:14 +0530

    vq-log: Introduced more indirection on VQ.

--- src/vt/utils/logging/logging/configurators.py
+++ src/vt/utils/logging/logging/configurators.py
@@ -5,10 +5,9 @@
 """
 Logger interfaces for Logger configurators.
 """
-
 import logging
 from abc import abstractmethod
-from typing import Protocol, Literal
+from typing import Protocol, Literal, override, get_args, Any
 
 from vt.utils.logging.logging.std_log.base import DirectStdAllLevelLogger
 from vt.utils.logging.warnings import vt_warn
@@ -41,19 +40,189 @@ Quietness literal. Progressively denotes more and more quietness.
 type VQ_DICT_LITERAL[T] = dict[V_LITERAL | Q_LITERAL, T]
 """
 Literal denoting how should a {``verbosity-quietness -> logging-level``} dict should be structured.
+
+:param T: type of the logger level, for e.g. logger level type is [int | str] for python std logging lib.
 """
 
 
 class VQConfigurator[T](Protocol):
+    """
+    Configurator for verbosity and quietness configurations.
+    """
     @property
     @abstractmethod
     def vq_level_map(self) -> VQ_DICT_LITERAL[T]:
+        """
+        :return: A dictionary containing verbosity|quietness -> logging.level mapping.
+        """
+        ...
+
+
+class VQSepConfigurator[T](VQConfigurator[T], Protocol):
+    """
+    Configurator which takes verbosity and quietness separately (as separate arguments) for configuration.
+    """
+    @abstractmethod
+    def validate(self, verbosity: V_LITERAL | None, quietness: Q_LITERAL | None) -> bool:
+        """
+        Validate whether the supplied verbosity and quietness are valid.
+
+        :param verbosity: verbosity.
+        :param quietness: quietness.
+        :return: ``True`` if inputs are valid, ``False`` otherwise.
+        """
         ...
 
     @abstractmethod
-    def validate(self, verbosity: V_LITERAL, quietness: Q_LITERAL) -> bool:
+    def get_effective_level(self, verbosity: V_LITERAL | None, quietness: Q_LITERAL | None, default_level: T) -> T:
+        """
+        Get the effective level for supplied verbosity and quietness.
+
+        :param verbosity: verbosity.
+        :param quietness: quietness.
+        :param default_level: returned if both verbosity and quietness are ``None`` or not supplied.
+        :return: computed level for verbosity and quietness or ``default_level`` if both verbosity and quietness
+            are ``None``.
+        """
         ...
 
-    @abstractmethod
-    def get_effective_level(self, verbosity: V_LITERAL, quietness: Q_LITERAL, default_level: T | None = None) -> T:
+
+class VQSepExclusive[T](VQSepConfigurator[T]):
+    def __init__(self, vq_level_map: VQ_DICT_LITERAL[T], warn_only: bool = False):
+        """
+        Treats verbosity and quietness as separate and exclusive, i.e. both cannot be given together.
+
+        Treats such conditions as an error::
+
+            - verbosity and quietness are provided together.
+            - supplied verbosity or quietness is not within the ``vq_level_map``.
+
+        :param vq_level_map: A dictionary containing verbosity|quietness -> logging.level mapping.
+        :param warn_only: Only warn on potential errors instead of raising an Error.
+        """
+        self._vq_level_map = vq_level_map
+        self.warn_only = warn_only
+
+    @override
+    @property
+    def vq_level_map(self) -> VQ_DICT_LITERAL[T]:
+        return self._vq_level_map
+
+    @override
+    def validate(self, verbosity: V_LITERAL | None, quietness: Q_LITERAL | None) -> bool:
+        """
+        ``verbosity`` and ``quietness`` are not accepted together.
+
+        Examples::
+
+        Raise error if ``warn_only`` is ``False`` or not provided::
+
+        >>> VQSepExclusive({}).validate('v', 'q')
+        Traceback (most recent call last):
         ...
+        ValueError: 'verbosity' and 'quietness' cannot be given together.
+
+        Only warn if ``warn_only`` is provided ``True``::
+
+        >>> import sys
+        >>> import contextlib
+        >>> import warnings
+        >>> with warnings.catch_warnings():
+        ...     with contextlib.redirect_stderr(sys.stdout):
+        ...         VQSepExclusive({}, True).validate('v', 'q')
+        UserWarning: 'verbosity' and 'quietness' cannot be given together.
+        False
+
+        Return ``True`` if only one verbosity is supplied::
+
+        >>> VQSepExclusive({}).validate('v', None)
+        True
+
+        Return ``True`` if only one quietness is supplied::
+
+        >>> VQSepExclusive({}).validate(None, 'q')
+        True
+
+        Return ``True`` if both are ``None``::
+
+        >>> VQSepExclusive({}).validate(None, None)
+        True
+
+        :raise ValueError: If ``self.warn_only`` is ``False`` - if both verbosity and quietness are given.
+        :return: If ``self.warn_only`` is ``True`` - ``True`` if inputs are valid, ``False`` otherwise.
+        """
+        if verbosity and quietness:
+            warn_str = "'verbosity' and 'quietness' cannot be given together."
+            if self.warn_only:
+                vt_warn(warn_str)
+                return False
+            else:
+                raise ValueError(warn_str)
+        else:
+            return True
+
+    @override
+    def get_effective_level(self, verbosity: V_LITERAL | None, quietness: Q_LITERAL | None, default_level: T) -> T:
+        """
+        Get effective level by treating verbosity and quietness as separate entities.
+
+        Note::
+
+            verbosity and quietness are not to be provided together.
+
+        Examples::
+
+        Both verbosity and quietness provided together::
+
+        Not provided ``warn_only`` or is ``False``::
+
+        >>> VQSepExclusive({}).get_effective_level('v', 'q', 10)
+        Traceback (most recent call last):
+        ...
+        ValueError: 'verbosity' and 'quietness' cannot be given together.
+
+        Only warn if ``warn_only`` is provided ``True``::
+
+        >>> import sys
+        >>> import contextlib
+        >>> import warnings
+        >>> with warnings.catch_warnings():
+        ...     with contextlib.redirect_stderr(sys.stdout):
+        ...         VQSepExclusive[int]({'v': 20}, True).get_effective_level('v', 'q', 10)
+        UserWarning: 'verbosity' and 'quietness' cannot be given together.
+        10
+
+        :param verbosity: verbosity.
+        :param quietness: quietness.
+        :param default_level: level to return when verbosity and quietness are not in the ``vq_level_map``.
+        :returns: corresponding logging level according to verbosity and quietness calculations.
+            ``default_level`` if both verbosity and quietness are ``None`` or not supplied.
+            ``default_level`` if both are not supplied and ``warn_only`` is ``True``.
+        :rtype T: a type
+        :raise KeyError: if verbosity and quietness are absent in ``vq_level_map`` and ``warn_only`` is ``False``.
+        :raise ValueError: If both verbosity and quietness are given and ``self.warn_only`` is ``False``.
+        """
+        if not self.validate(verbosity, quietness):
+            level = default_level
+        else:
+            if verbosity:
+                level = self.level_or_default(verbosity, 'verbosity', default_level, get_args(V_LITERAL))
+            elif quietness:
+                level = self.level_or_default(quietness, 'quietness', default_level, get_args(Q_LITERAL))
+            else:
+                level = default_level
+        return level
+
+    def level_or_default(self, ver_qui: V_LITERAL | Q_LITERAL | None, emphasis_str: str, default_level: T,
+                         choices: tuple[Any, ...]) -> T:
+        if ver_qui:
+            try:
+                return self.vq_level_map[ver_qui]
+            except KeyError:
+                if self.warn_only:
+                    vt_warn(f"Unexpected {emphasis_str} value: {ver_qui}. Choose from {choices}.")
+                    return self.vq_level_map.get(ver_qui, default_level)
+                else:
+                    raise
+        else:
+            return default_level
