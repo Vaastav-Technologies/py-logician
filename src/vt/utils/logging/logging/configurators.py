61bad0af0fbd3074c163b77808e7bd8234bb28a6
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-10 10:56:58 +0530

    vq-log: `VQCommon` defined.
    This class treats verbosity and quietness as one argument.

--- src/vt/utils/logging/logging/configurators.py
+++ src/vt/utils/logging/logging/configurators.py
@@ -7,7 +7,7 @@ Logger interfaces for Logger configurators.
 """
 import logging
 from abc import abstractmethod
-from typing import Protocol, Literal, override, get_args, Any
+from typing import Protocol, Literal, override, Any
 
 from vt.utils.logging.logging.std_log.base import DirectStdAllLevelLogger
 from vt.utils.logging.warnings import vt_warn, Warner
@@ -378,7 +378,7 @@ class VQSepExclusive[T](VQSepConfigurator[T]):
             >>> with warnings.catch_warnings():
             ...     with contextlib.redirect_stderr(sys.stdout):
             ...         VQSepExclusive[int]({'v': 20}, True).get_effective_level('vv', None, 10)
-            UserWarning: 'vv': Unexpected verbosity value. Choose from ('v', 'vv', 'vvv').
+            UserWarning: 'vv': Unexpected verbosity value. Choose from ['v'].
             10
 
             Raise KeyError if queried verbosity is not registered and warn_only is False or not provided:
@@ -386,7 +386,7 @@ class VQSepExclusive[T](VQSepConfigurator[T]):
             >>> VQSepExclusive[int]({'v': 20}).get_effective_level('vv', None, 10)
             Traceback (most recent call last):
             ...
-            KeyError: "'vv': Unexpected verbosity value. Choose from ('v', 'vv', 'vvv')."
+            KeyError: "'vv': Unexpected verbosity value. Choose from ['v']."
 
         :param verbosity: verbosity.
         :param quietness: quietness.
@@ -411,4 +411,107 @@ class VQSepExclusive[T](VQSepConfigurator[T]):
                                                                        list(self.vq_level_map.keys()))
             else:
                 level = default_level
+        return level
+
+
+class VQCommon[T](VQCommConfigurator[T]):
+    def __init__(self, vq_level_map: VQ_DICT_LITERAL[T], warn_only: bool = False,
+                 level_or_default_handler: VQLevelOrDefault[T] | None = None):
+        """
+        Treats verbosity and quietness as on inclusive argument.
+
+        Treats such conditions as an error::
+
+            - supplied verbosity or quietness is not within the ``vq_level_map``.
+
+        :param vq_level_map: A dictionary containing verbosity|quietness -> logging.level mapping.
+        :param warn_only: Only warn on potential errors instead of raising an Error.
+        """
+        self._vq_level_map = vq_level_map
+        self.warn_only = warn_only
+        if level_or_default_handler:
+            self.level_or_default_handler = level_or_default_handler
+        else:
+            self.level_or_default_handler = SimpleWarningVQLevelOrDefault(vq_level_map, warn_only)
+
+    @override
+    @property
+    def vq_level_map(self) -> VQ_DICT_LITERAL[T]:
+        return self._vq_level_map
+
+    @override
+    def validate(self, ver_qui: V_LITERAL | Q_LITERAL | None) -> bool:
+        """
+        Examples::
+
+        >>> VQCommon({}).validate('v')
+        False
+
+
+        >>> VQCommon({}).validate('q')
+        False
+
+        >>> VQCommon({}).validate('vqn') # noqa
+        False
+
+        >>> VQCommon[int]({'v': 20}).validate('v')
+        True
+
+
+        >>> VQCommon({'v': 20, 'q': 1}).validate('q')
+        True
+
+        :return: ``True`` if ``ver_qui`` is in the assigned level map, else ``False``.
+        """
+        if ver_qui in self.vq_level_map:
+            return True
+        return False
+
+    @override
+    def get_effective_level(self, ver_qui: V_LITERAL | Q_LITERAL | None, default_level: T) -> T:
+        """
+        Get effective level by treating verbosity and quietness as single argument.
+
+        Examples::
+
+        >>> import sys
+        >>> import contextlib
+        >>> import warnings
+
+        Level inquiry::
+
+            Get queried verbosity:
+
+            >>> VQCommon[int]({'v': 20}).get_effective_level('v', 10)
+            20
+
+            Return default_level if queried verbosity is not registered and warn_only is True:
+
+            >>> with warnings.catch_warnings():
+            ...     with contextlib.redirect_stderr(sys.stdout):
+            ...         VQCommon[int]({'v': 20, 'q': 1}, True).get_effective_level('vv', 10)
+            UserWarning: 'vv': Unexpected verbosity or quietness value. Choose from ['v', 'q'].
+            10
+
+            Raise KeyError if queried verbosity or quietness is not registered and warn_only is False or not provided:
+
+            >>> VQCommon[int]({'v': 20, 'qqq': 70}).get_effective_level('qq', 10)
+            Traceback (most recent call last):
+            ...
+            KeyError: "'qq': Unexpected verbosity or quietness value. Choose from ['v', 'qqq']."
+
+        :param ver_qui: `verbosity` or `quietness`.
+        :param default_level: level to return when verbosity and quietness are not in the ``vq_level_map``.
+        :returns: corresponding logging level according to verbosity and quietness calculations.
+            ``default_level`` if both verbosity and quietness are ``None`` or not supplied.
+            ``default_level`` if both are not supplied and ``warn_only`` is ``True``.
+        :rtype T: a type
+        :raise KeyError: if verbosity and quietness are absent in ``vq_level_map`` and ``warn_only`` is ``False``.
+        """
+        if ver_qui:
+            level = self.level_or_default_handler.level_or_default(ver_qui,
+                                                                   'verbosity or quietness', default_level,
+                                                                   list(self.vq_level_map.keys()))
+        else:
+            level = default_level
         return level
