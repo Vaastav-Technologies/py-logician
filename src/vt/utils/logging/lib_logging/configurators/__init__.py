cffc89047dcb119768996f2a11d6fe649ee5feea
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-18 20:13:33 +0530

    all-log: docs added.

--- src/vt/utils/logging/lib_logging/configurators/__init__.py
+++ src/vt/utils/logging/lib_logging/configurators/__init__.py
@@ -71,6 +71,12 @@ class LevelLoggerConfigurator[T](LevelTarget[T], LoggerConfigurator, Protocol):
 
 class SupplierLoggerConfigurator[T](LoggerConfigurator):
     def __init__(self, level_supplier: Callable[[], T], configurator: LevelLoggerConfigurator[T]):
+        """
+        Configurator that configures loggers as per the level supplied by the ``level_supplier``.
+
+        :param level_supplier: a supplier to supply level.
+        :param configurator: underlying configurator.
+        """
         self.level_supplier = level_supplier
         self.configurator = configurator
 
@@ -82,8 +88,17 @@ class SupplierLoggerConfigurator[T](LoggerConfigurator):
 
 class ListLoggerConfigurator[T](LoggerConfigurator):
 
-    def __init__(self, level_list: list[T], configurator: LevelLoggerConfigurator,
+    def __init__(self, level_list: list[T | None], configurator: LevelLoggerConfigurator,
                  level_pickup_strategy =get_first_non_none):
+        """
+        Picks up the first non ``None`` level from the ``level_list`` to configure the logger underneath.
+
+        :param level_list: list of log levels which may contain ``None``. First non-``None`` value
+            is picked-up by default for logger configuration.
+        :param configurator: configurator which is decorated by this logger-configurator.
+        :param level_pickup_strategy: pick up a level from the list of levels supplied in ``level_list``. Default is
+            to pick up the first non-``None`` level.
+        """
         if not level_list:
             raise ValueError("Level list must not be None or empty.")
         self.level_list = level_list
@@ -100,6 +115,17 @@ class ListLoggerConfigurator[T](LoggerConfigurator):
 class EnvListLC[T](ListLoggerConfigurator):
     def __init__(self, env_list: list[str], configurator: LevelLoggerConfigurator[T],
                  level_pickup_strategy =get_first_non_none):
+        """
+        This logger configurator can be used to configure log level using values supplied from environment variables.
+        Default behavior is to pick up the first passed environment variable value. Designed to process log level from
+        multiple environment variables and hence has a precedence order to the values form environment variables. The
+        first environment variable value takes highest precedence and then the precedence diminishes.
+
+        :param env_list: list of environment variables. Default behavior is to take precedence in decreasing order.
+        :param configurator: underlying logger configurator.
+        :param level_pickup_strategy: strategy to pick-up level from a supplied list of levels. Default is to pick up
+            the first supplied, then next and then so on.
+        """
         super().__init__([os.getenv(e) for e in env_list], configurator, level_pickup_strategy)
         self._env_list = env_list
 
@@ -110,5 +136,18 @@ class EnvListLC[T](ListLoggerConfigurator):
 class VTEnvListLC[T](EnvListLC[T]):
     def __init__(self, env_list: list[str], configurator: LevelLoggerConfigurator[T],
                  level_pickup_strategy=get_first_non_none):
+        """
+        This logger configurator can be used to configure log level using values supplied from environment variables.
+        Default behavior is to pick up the first passed environment variable value. Designed to process log level from
+        multiple environment variables and hence has a precedence order to the values form environment variables. The
+        first environment variable value takes highest precedence and then the precedence diminishes. Environment
+        variable ``VT_ALL_LOG`` is always appended to ``env_list`` so that if no environment variable is registered
+        then at least this one is registered.
+
+        :param env_list: list of environment variables. Default behavior is to take precedence in decreasing order.
+        :param configurator: underlying logger configurator.
+        :param level_pickup_strategy: strategy to pick-up level from a supplied list of levels. Default is to pick up
+            the first supplied, then next and then so on.
+        """
         env_list.append(VT_ALL_LOG_ENV_VAR)
         super().__init__(env_list, configurator, level_pickup_strategy)
